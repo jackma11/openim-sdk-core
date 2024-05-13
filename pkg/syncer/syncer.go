2aa524ec5fbfc6de4e48ba73cfe48428baf29c17
brooks <brooks12580@gmail.com>
2024-05-13 13:10:25 +0800

    修复本地数据不存在发送时间小于 历史记录

--- pkg/syncer/syncer.go
+++ pkg/syncer/syncer.go
@@ -127,7 +127,7 @@ func (s *Syncer[T, V]) Sync(ctx context.Context, serverData []T, localData []T, 
 	for i, item := range localData {
 		localMap[s.uuid(item)] = localData[i]
 	}
-
+	log.ZWarn(ctx, "Sync-serverData", err, "localMap", localMap, "serverData", serverData)
 	// Iterate through server data to sync with local data.
 	for i := range serverData {
 		server := serverData[i]
